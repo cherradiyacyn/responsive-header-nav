9add02547768db0720d72bb2855f6e3fcde78320
Yacyn CHERRADI <cherradiyacyn@localhost.localdomain>
2024-01-17 00:27:47 +0100

    better naming

--- js/header.js
+++ js/header.js
@@ -1,14 +1,15 @@
-const header__hamburger = document.querySelector(".header__hamburger");
-const header__menu = document.querySelector(".header__menu");
-const menu__container = document.querySelector(".menu__container");
-const menu__closer = document.querySelector(".menu__closer");
+// const name = document.querySelector(".BEM");
+const headerHamburger = document.querySelector(".header__hamburger");
+const headerMenu = document.querySelector(".header__menu");
+const menuContainer = document.querySelector(".menu__container");
+const menuCloser = document.querySelector(".menu__closer");
 
-// handle__ = () => {};
-handle__header__menu = () => {
-  header__menu.classList.toggle("hidden");
+// const handlerName = () => {};
+const handleHeaderMenu = () => {
+  headerMenu.classList.toggle("hidden");
 };
 
-// .addEventListener("click", handle__);
-header__hamburger.addEventListener("click", handle__header__menu);
-menu__container.addEventListener("click", handle__header__menu);
-menu__closer.addEventListener("click", handle__header__menu);
+// name.addEventListener("click", handlerName);
+headerHamburger.addEventListener("click", handleHeaderMenu);
+menuContainer.addEventListener("click", handleHeaderMenu);
+menuCloser.addEventListener("click", handleHeaderMenu);
