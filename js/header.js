f4844372e6f34e1211a414a0b182e9a3d22e4e2d
Yacyn CHERRADI <cherradiyacyn@gmail.com>
2024-01-22 16:44:19 +0100

    enable/disable event listener when media change

--- js/header.js
+++ js/header.js
@@ -1,16 +1,33 @@
-// const name = document.querySelector(".BEM");
 const navHamburger = document.querySelector(".nav__hamburger");
-const navInnerWrapper = document.querySelector(".nav__inner-wrapper");
+const navWrapper = document.querySelector(".nav__wrapper");
 const navMenu = document.querySelector(".nav__menu");
 const navCloser = document.querySelector(".nav__closer");
 
-// const handlerName = () => {};
-const handlenavInnerWrapper = () => {
-  navInnerWrapper.classList.toggle("hidden");
-  // alert("FIX IT LATER : this should not work in desktop version!");
+const mql = window.matchMedia("(min-width: 768px)");
+
+function initMobileFirst() {
+  navWrapper.classList.add("hide");
+}
+
+const handleNavWrapper = () => {
+  navWrapper.classList.toggle("hide");
 };
 
-// name.addEventListener("click", handlerName);
-navHamburger.addEventListener("click", handlenavInnerWrapper);
-navMenu.addEventListener("click", handlenavInnerWrapper);
-navCloser.addEventListener("click", handlenavInnerWrapper);
+function handleMedia() {
+  if (mql.matches) {
+    navWrapper.classList.remove("hide");
+    navHamburger.removeEventListener("click", handleNavWrapper);
+    navMenu.removeEventListener("click", handleNavWrapper);
+    navCloser.removeEventListener("click", handleNavWrapper);
+  } else {
+    navWrapper.classList.add("hide");
+    navHamburger.addEventListener("click", handleNavWrapper);
+    navMenu.addEventListener("click", handleNavWrapper);
+    navCloser.addEventListener("click", handleNavWrapper);
+  }
+}
+
+mql.addEventListener("change", handleMedia);
+
+initMobileFirst();
+handleMedia();
